3fc303ba496700d1848c8f732a636d5f0457f80e
William Casarin <bill@casarin.me>
2015-03-25 16:37:36 -0700

    docs, csv support, etc

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-var debug = require('debug')('mongomerge');
+var debug = require('debug')('mongomerge')
 
-function handle(collection, data, opts, cb){
-  var updated = 0;
+function handle (collection, data, opts, cb) {
   if (cb == null) {
     cb = opts
     opts = {}
@@ -9,27 +8,45 @@ function handle(collection, data, opts, cb){
 
   opts = opts || {}
 
-  var update = { $set: {} };
-  var query = { _id: data._id };
+  var update = { $set: {} }
+
+  var key = opts.key || '_id'
+  var query = {}
+  query[key] = data[key]
 
   Object.keys(data).forEach(function (prop) {
     var elem = data[prop]
-    if (!(elem === "" && opts.ignoreEmpty))
+    if (!(elem === '' && opts.ignoreEmpty)) {
       update.$set[prop] = elem
+    }
   })
 
-  delete update.$set._id;
+  delete update.$set[key]
 
-  debug("query %j update %j", query, update);
+  debug('query %j update %j', query, update)
 
   if (opts.dryRun) {
-    console.log("(dry run) would update %s with %j", data._id.toString(), update)
-    return cb && cb(null, 0)
+    collection.findOne(query, function (err, doc) {
+      var keyVal = data[key]
+      if (err) {
+        console.log("(dry run) would NOT update '%s', got error %s", keyVal, err)
+        return cb && cb(null, { n: 0 })
+      }
+
+      if (doc == null) {
+        console.log("(dry run) would NOT update '%s', key not found", keyVal)
+        return cb && cb(null, { n: 0 })
+      }
+
+      console.log("(dry run) would update '%s' with %j", keyVal, update)
+      return cb && cb(null, { n: 0 })
+    })
+    return
   }
 
-  collection.update(query, update, { multi: false }, function(err, doc, other){
-    cb && cb(err, doc, other);
-  });
+  collection.update(query, update, { multi: false }, function (err, doc, other) {
+    cb && cb(err, doc, other)
+  })
 }
 
 module.exports = handle
